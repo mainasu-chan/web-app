f1a4f688243b50d0c0f25f9c7f458e0ae5b2d170
mainasu-chan <166491905+mainasu-chan@users.noreply.github.com>
2025-04-22 13:52:46 +0900

    二回目

---
+++ www/3/mon7.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>問題7</title>
+    </head>
+    <body>
+        <?php
+            $base = 10;
+            $height = 20;
+
+            $area = $base * $height / 2;
+            echo '底辺'.$base.'cm高さ'.$height.'cmの三角形の面積は'.$area.'cm<sup>2</sup>です';
+        ?>
+    </body>
+</html>
